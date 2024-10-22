ed4b870d836dec34711abeb57a219f036a1e8b66
Yarden <yarden.as@inf.ethz.ch>
2024-10-22 14:54:57 +0200

    mass dr

--- ss2r/benchmark_suites/brax/__init__.py
+++ ss2r/benchmark_suites/brax/__init__.py
@@ -1,7 +1,7 @@
 from ss2r.benchmark_suites.brax.cartpole import cartpole
 
 randomization_fns = {
-    "cartpole_swingup": cartpole.domain_randomization_length,
+    "cartpole_swingup": cartpole.domain_randomization,
     "cartpole_swingup_sparse": cartpole.domain_randomization,
     "cartpole_balance": cartpole.domain_randomization,
     "inverted_pendulum": cartpole.domain_randomization,
