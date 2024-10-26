0583b852f2f13d25f1aac11395f903640915452c
Yarden <yarden.as@inf.ethz.ch>
2024-10-26 15:56:25 +0200

    Add videos

--- ss2r/benchmark_suites/brax/__init__.py
+++ ss2r/benchmark_suites/brax/__init__.py
@@ -6,7 +6,7 @@ from ss2r.rl.utils import rollout
 
 
 def render(env, policy, steps, rng):
-    _, trajectory = rollout(policy, steps, rng)
+    _, trajectory = rollout(env, policy, steps, rng)
     trajectory = jax.tree_map(lambda x: x[:, 0], trajectory.extras["pipeline_state"])
     trajectory = pytrees_unstack(trajectory)
     video = image.render_array(env.sys, trajectory)
--- ss2r/rl/utils.py
+++ ss2r/rl/utils.py
@@ -1,29 +1,29 @@
 import jax
-from brax.training.types import Policy, State
+from brax.envs import Env, State
+from brax.training.types import Policy
 
 
 def rollout(
-    self,
+    env: Env,
     policy: Policy,
     steps: int,
-    rng: int,
+    rng: jax.Array,
     state: State | None = None,
 ) -> tuple[State, State]:
+    parallel_envs = env._sys_v.m.shape
+    keys = jax.random.split(rng, parallel_envs[0])
     if state is None:
-        rng, rng = jax.random.split(rng)
-        keys = jax.random.split(rng, self.parallel_envs)
-        state = self.environment.reset(keys)
+        state = env.reset(keys)
 
     def f(carry, _):
         state, current_key = carry
         current_key, next_key = jax.random.split(current_key)
-        nstate, transition = self.step(
+        action, _ = policy(state.obs, current_key)
+        nstate = env.step(
             state,
-            policy,
-            current_key,
-            extra_fields=("truncation",),
+            action,
         )
-        return (nstate, next_key), transition
+        return (nstate, next_key), nstate
 
     (final_state, _), data = jax.lax.scan(f, (state, rng), (), length=steps)
     return final_state, data
