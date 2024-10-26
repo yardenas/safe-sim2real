f31d0dd7cc8ee7a72aa52418be8fff1a9ab42fa7
Yarden As <yarden.as@inf.ethz.ch>
2024-10-26 16:07:44 +0200

    Video (#9)
    * Cleanup
    
    * Add implementation for video
    
    * Add videos

--- tests/test_cartpole.py
+++ tests/test_cartpole.py
@@ -14,17 +14,6 @@ from ss2r import benchmark_suites
 from tests import make_test_config
 
 
-def pytrees_unstack(pytree):
-    leaves, treedef = jax.tree_flatten(pytree)
-    n_trees = leaves[0].shape[0]
-    new_leaves = [[] for _ in range(n_trees)]
-    for leaf in leaves:
-        for i in range(n_trees):
-            new_leaves[i].append(leaf[i])
-    new_trees = [treedef.unflatten(leaf) for leaf in new_leaves]
-    return new_trees
-
-
 @pytest.mark.skip
 def test_sac():
     train_fn = functools.partial(
