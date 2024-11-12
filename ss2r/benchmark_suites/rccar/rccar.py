dd81dda4666283d21f46b6e3f18f97c98fcb9b44
Yarden <yarden.as@inf.ethz.ch>
2024-11-12 22:49:48 +0100

    No done

--- ss2r/benchmark_suites/rccar/rccar.py
+++ ss2r/benchmark_suites/rccar/rccar.py
@@ -237,7 +237,7 @@ class RCCar(Env):
             nkey,
         )
         cost = cost_fn(next_obs[..., 2:4], self.obstacles)
-        done = 1.0 - in_arena(next_obs[..., 2:4], 1.2)
+        done = jnp.zeros_like(1.0 - in_arena(next_obs[..., 2:4], 1.2))
         info = {**state.info, "cost": cost, **step_info}
         next_state = State(
             pipeline_state=(next_dynamics_state, goal, nkey, goal_dist),
