b400eb27ecd03031ed62c4fd69668a7237bee310
Yarden As <yarden.as@inf.ethz.ch>
2024-11-19 10:51:24 +0100

    Add layer normalization to q function (#22)
    * Add layer normalization to q function
    
    * Add softplus
    
    * Fix bug
    
    * Project multiplier

--- ss2r/algorithms/sac/networks.py
+++ ss2r/algorithms/sac/networks.py
@@ -14,12 +14,18 @@
 
 """SAC networks."""
 
-from typing import Protocol, Sequence, TypeVar
+from typing import Any, Callable, Protocol, Sequence, TypeVar
 
 import brax.training.agents.sac.networks as sac_networks
 import flax
+import jax
+import jax.nn as jnn
+import jax.numpy as jnp
 from brax.training import distribution, networks, types
 from flax import linen
+
+ActivationFn = Callable[[jnp.ndarray], jnp.ndarray]
+Initializer = Callable[..., Any]
 
 make_inference_fn = sac_networks.make_inference_fn
 NetworkType = TypeVar("NetworkType", covariant=True)
@@ -46,6 +52,62 @@ class DomainRandomizationNetworkFactory(Protocol[NetworkType]):
         pass
 
 
+class MLP(linen.Module):
+    layer_sizes: Sequence[int]
+    activation: Callable
+    kernel_init: Callable = jax.nn.initializers.lecun_uniform()
+
+    @linen.compact
+    def __call__(self, x):
+        for i, size in enumerate(self.layer_sizes):
+            x = linen.Dense(features=size, kernel_init=self.kernel_init)(x)
+            if i < len(self.layer_sizes) - 1:
+                x = linen.LayerNorm()(x)
+                x = self.activation(x)
+        return x
+
+
+def make_q_network(
+    obs_size: int,
+    action_size: int,
+    preprocess_observations_fn: types.PreprocessObservationFn = types.identity_observation_preprocessor,
+    hidden_layer_sizes: Sequence[int] = (256, 256),
+    activation: ActivationFn = linen.relu,
+    n_critics: int = 2,
+) -> networks.FeedForwardNetwork:
+    """Creates a value network."""
+
+    class QModule(linen.Module):
+        """Q Module."""
+
+        n_critics: int
+
+        @linen.compact
+        def __call__(self, obs: jnp.ndarray, actions: jnp.ndarray):
+            hidden = jnp.concatenate([obs, actions], axis=-1)
+            res = []
+            for _ in range(self.n_critics):
+                q = MLP(
+                    layer_sizes=list(hidden_layer_sizes) + [1],
+                    activation=activation,
+                    kernel_init=jax.nn.initializers.lecun_uniform(),
+                )(hidden)
+                res.append(q)
+            return jnp.concatenate(res, axis=-1)
+
+    q_module = QModule(n_critics=n_critics)
+
+    def apply(processor_params, q_params, obs, actions):
+        obs = preprocess_observations_fn(obs, processor_params)
+        return q_module.apply(q_params, obs, actions)
+
+    dummy_obs = jnp.zeros((1, obs_size))
+    dummy_action = jnp.zeros((1, action_size))
+    return networks.FeedForwardNetwork(
+        init=lambda key: q_module.init(key, dummy_obs, dummy_action), apply=apply
+    )
+
+
 def make_sac_networks(
     observation_size: int,
     action_size: int,
@@ -67,7 +129,7 @@ def make_sac_networks(
         hidden_layer_sizes=hidden_layer_sizes,
         activation=activation,
     )
-    qr_network = networks.make_q_network(
+    qr_network = make_q_network(
         observation_size,
         action_size + domain_randomization_size,
         preprocess_observations_fn=preprocess_observations_fn,
@@ -75,12 +137,16 @@ def make_sac_networks(
         activation=activation,
     )
     if safe:
-        qc_network = networks.make_q_network(
+        qc_network = make_q_network(
             observation_size,
             action_size + domain_randomization_size,
             preprocess_observations_fn=preprocess_observations_fn,
             hidden_layer_sizes=hidden_layer_sizes,
             activation=activation,
+        )
+        old_apply = qc_network.apply
+        qc_network.apply = lambda *args, **kwargs: jnn.softplus(
+            old_apply(*args, **kwargs)
         )
     else:
         qc_network = None
