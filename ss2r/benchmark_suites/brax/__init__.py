fb69a79e949b28a90dad4461f71a16c722f461c7
Yarden <yarden.as@inf.ethz.ch>
2024-12-02 09:16:55 +0100

    Smaller images

--- ss2r/benchmark_suites/brax/__init__.py
+++ ss2r/benchmark_suites/brax/__init__.py
@@ -13,6 +13,8 @@ def render(env, policy, steps, rng, camera=None):
     for i in range(1):
         ep_trajectory = jax.tree_map(lambda x: x[:, i], trajectory.pipeline_state)
         ep_trajectory = pytrees_unstack(ep_trajectory)
-        video = image.render_array(env.sys, ep_trajectory, camera=camera)
+        video = image.render_array(
+            env.sys, ep_trajectory, camera=camera, height=120, width=160
+        )
         videos.append(video)
     return np.asarray(videos).transpose(0, 1, 4, 2, 3)
