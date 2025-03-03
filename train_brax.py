8a7e168ef0bcaba61457129af9de940e5265d7e1
Yarden <yarden.as@inf.ethz.ch>
2025-03-03 13:44:47 +0100

    Use ppo

--- train_brax.py
+++ train_brax.py
@@ -127,6 +127,26 @@ def get_train_fn(cfg):
             penalizer=penalizer,
             penalizer_params=penalizer_params,
         )
+    elif cfg.agent.name == "ppo":
+        from mujoco_playground.config import locomotion_params
+
+        ppo_params = locomotion_params.brax_ppo_config(cfg.environment.task_name)
+        from brax.training.agents.ppo import networks as ppo_networks
+        from brax.training.agents.ppo import train as ppo
+
+        ppo_training_params = dict(ppo_params)
+        network_factory = ppo_networks.make_ppo_networks
+        if "network_factory" in ppo_params:
+            del ppo_training_params["network_factory"]
+        network_factory = functools.partial(
+            ppo_networks.make_ppo_networks, **ppo_params.network_factory
+        )
+        train_fn = functools.partial(
+            ppo.train,
+            **dict(ppo_training_params),
+            network_factory=network_factory,
+            wrap_env=False,
+        )
     else:
         raise ValueError(f"Unknown agent name: {cfg.agent.name}")
     return train_fn
