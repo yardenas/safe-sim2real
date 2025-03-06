a6230c4e7ea1251812b5220eee8f73890629936f
Yarden As <yarden.as@inf.ethz.ch>
2025-03-06 17:17:18 +0100

    Add bro (#49)
    * Add bro
    
    * Fix bug in gradients with adamw
    
    * Use bro
    
    * Reset every now and then
    
    * Separate policy and value sizes
    
    * Use bro also on critic loss
    
    * Larger value network
    
    * float16 in buffer
    
    * Last layer of bro

--- ss2r/benchmark_suites/mujoco_playground/go1_joystick/go1_joystick.py
+++ ss2r/benchmark_suites/mujoco_playground/go1_joystick/go1_joystick.py
@@ -3,19 +3,21 @@ from mujoco_playground import locomotion
 
 
 def domain_randomization(sys, rng, cfg):
-    model, in_axes = locomotion.domain_randomization(sys, rng, cfg)
-    new_pair_friction = model.pair_friction[:, :2, 0:2]
-    new_dof_frictionloss = model.dof_frictionloss[6:]
-    new_armature = model.dof_armature[6:]
-    new_body_mass = model.body_mass
-    new_qpos0 = model.qpos0[7:]
-    samples = jnp.stack(
+    model, in_axes = locomotion.go1_randomize.domain_randomize(sys, rng)
+    geom_friction = model.geom_friction[:, 0, 0]
+    body_ipos = model.body_ipos[:, 1]
+    body_mass = model.body_mass
+    qpos0 = model.qpos0[:, 7:]
+    dof_frictionloss = model.dof_frictionloss[:, 6:]
+    dof_armature = model.dof_armature[:, 6:]
+    samples = jnp.hstack(
         [
-            new_pair_friction,
-            new_dof_frictionloss,
-            new_armature,
-            new_body_mass,
-            new_qpos0,
+            geom_friction[:, None],
+            body_ipos,
+            body_mass,
+            qpos0,
+            dof_frictionloss,
+            dof_armature,
         ],
     )
     return model, in_axes, samples
