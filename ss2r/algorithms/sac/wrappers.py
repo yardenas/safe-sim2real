d6c8f7d9a5b28981cef1ade77532df28c09fd0ad
Yarden As <yarden.as@inf.ethz.ch>
2025-03-08 10:09:13 +0100

    Go1 dr (#66)
    * Add domain randomization for go1
    
    * Use episodic propagation
    
    * Reset every now and then
    
    * Float32
    
    * float16 in buffer
    
    * Update porting script
    
    * Load wrappers beforehand
    
    * Update playground

--- ss2r/algorithms/sac/wrappers.py
+++ ss2r/algorithms/sac/wrappers.py
@@ -5,6 +5,8 @@ import jax
 import jax.numpy as jnp
 from brax.envs import State, Wrapper
 
+from ss2r.benchmark_suites.mujoco_playground import BraxDomainRandomizationVmapWrapper
+
 
 class PropagationFn(Protocol):
     def __call__(self, state: State, rng: jax.Array) -> State:
@@ -12,18 +14,48 @@ class PropagationFn(Protocol):
 
 
 def ts1(state, rng):
-    num_envs = _get_state(state).shape[0]
+    num_envs = _get_obs(state).shape[0]
     id_ = jax.random.randint(rng, (), 0, num_envs)
     sampled_state = jax.tree_map(lambda x: x[id_], state, is_leaf=eqx.is_array)
     return sampled_state
 
 
-def _get_state(state):
+def _get_obs(state):
     if isinstance(state.obs, jax.Array):
         return state.obs
     else:
         assert isinstance(state.obs, Mapping)
         return state.obs["state"]
+
+
+class PTSD(Wrapper):
+    def __init__(self, env, randomzation_fn, num_perturbed_envs):
+        super().__init__(env)
+        # FIXME (yarden): this should be the nominal environment.
+        self.perturbed_env = BraxDomainRandomizationVmapWrapper(env, randomzation_fn)
+        self.num_perturbed_envs = num_perturbed_envs
+
+    def reset(self, rng: jax.Array) -> State:
+        # No need to randomize the initial state. Otherwise, even without
+        # domain randomization, the initial states will be different, having
+        # a non-zero disagreement.
+        state = self.env.reset(rng)
+        state.info["state_propagation"] = {}
+        state.info["state_propagation"]["next_obs"] = self._tile(_get_obs(state))
+        return state
+
+    def step(self, state: State, action: jax.Array) -> State:
+        nstate = self.env.step(state, action)
+        v_state, v_action = self._tile(state), self._tile(action)
+        perturbed_nstate = self.perturbed_env.step(v_state, v_action)
+        next_obs = _get_obs(perturbed_nstate)
+        nstate.info["state_propagation"]["next_obs"] = next_obs
+        return nstate
+
+    def _tile(self, tree):
+        return jax.tree_map(
+            lambda x: jnp.tile(x, (self.num_perturbed_envs,) + (1,) * x.ndim), tree
+        )
 
 
 class StatePropagation(Wrapper):
@@ -50,7 +82,7 @@ class StatePropagation(Wrapper):
         n_key, key = jax.random.split(propagation_rng)
         state.info["state_propagation"] = {}
         state.info["state_propagation"]["rng"] = jax.random.split(n_key, self.num_envs)
-        orig_next_obs = _get_state(state)
+        orig_next_obs = _get_obs(state)
         state = self.propagation_fn(state, key)
         state.info["state_propagation"]["next_obs"] = orig_next_obs
         return state
@@ -65,7 +97,7 @@ class StatePropagation(Wrapper):
         state, action = tile(state), tile(action)
         nstate = self.env.step(state, action)
         n_key, key = jax.random.split(propagation_rng)
-        orig_next_obs = _get_state(nstate)
+        orig_next_obs = _get_obs(nstate)
         nstate.info["state_propagation"]["rng"] = jax.random.split(n_key, self.num_envs)
         nstate.info["state_propagation"]["next_obs"] = nstate.obs
         nstate = self.propagation_fn(nstate, key)
@@ -76,7 +108,6 @@ class StatePropagation(Wrapper):
 class ModelDisagreement(Wrapper):
     def __init__(self, env):
         super().__init__(env)
-        assert isinstance(env.env, StatePropagation)
 
     def reset(self, rng: jax.Array) -> State:
         state = self.env.reset(rng)
