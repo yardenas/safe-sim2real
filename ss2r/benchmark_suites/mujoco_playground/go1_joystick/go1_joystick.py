45f70262d3d798926ed9ba59d719f8ba30ef5258
Yarden <yarden.as@inf.ethz.ch>
2025-03-08 11:51:51 +0100

    Cleanup

--- ss2r/benchmark_suites/mujoco_playground/go1_joystick/go1_joystick.py
+++ ss2r/benchmark_suites/mujoco_playground/go1_joystick/go1_joystick.py
@@ -1,32 +1,32 @@
 import jax
 import jax.numpy as jnp
-from brax.envs import Wrapper
+from brax.envs import Env, State, Wrapper
 from mujoco_playground import locomotion
-from brax.envs.base import Wrapper, Env, State
 
 
 def domain_randomization(sys, rng, cfg):
-
     FLOOR_GEOM_ID = 0
     TORSO_BODY_ID = 1
     model = sys
 
     @jax.vmap
     def rand_dynamics(rng):
-        # Floor friction: 
+        # Floor friction:
         rng, key = jax.random.split(rng)
         geom_friction = model.geom_friction.at[FLOOR_GEOM_ID, 0].set(
-            jax.random.uniform(key, minval=cfg.floor_friction[0], maxval=cfg.floor_friction[1])
+            jax.random.uniform(
+                key, minval=cfg.floor_friction[0], maxval=cfg.floor_friction[1]
+            )
         )
 
-        # Scale static friction: 
+        # Scale static friction:
         rng, key = jax.random.split(rng)
         frictionloss = model.dof_frictionloss[6:] * jax.random.uniform(
             key, shape=(12,), minval=cfg.scale_friction[0], maxval=cfg.scale_friction[1]
         )
         dof_frictionloss = model.dof_frictionloss.at[6:].set(frictionloss)
 
-        # Scale armature: 
+        # Scale armature:
         rng, key = jax.random.split(rng)
         armature = model.dof_armature[6:] * jax.random.uniform(
             key, shape=(12,), minval=cfg.scale_armature[0], maxval=cfg.scale_armature[1]
@@ -35,31 +35,38 @@ def domain_randomization(sys, rng, cfg):
 
         # Jitter center of mass positiion:
         rng, key = jax.random.split(rng)
-        dpos = jax.random.uniform(key, (3,), minval=cfg.jitter_mass[0], maxval=cfg.jitter_mass[0])
+        dpos = jax.random.uniform(
+            key, (3,), minval=cfg.jitter_mass[0], maxval=cfg.jitter_mass[0]
+        )
         body_ipos = model.body_ipos.at[TORSO_BODY_ID].set(
             model.body_ipos[TORSO_BODY_ID] + dpos
         )
 
-        # Scale all link masses: 
+        # Scale all link masses:
         rng, key = jax.random.split(rng)
         dmass = jax.random.uniform(
-            key, shape=(model.nbody,), minval=cfg.scale_link_mass[0], maxval=cfg.scale_link_mass[1]
+            key,
+            shape=(model.nbody,),
+            minval=cfg.scale_link_mass[0],
+            maxval=cfg.scale_link_mass[1],
         )
         body_mass = model.body_mass.at[:].set(model.body_mass * dmass)
 
         # Add mass to torso:
         rng, key = jax.random.split(rng)
-        dmass = jax.random.uniform(key, minval=cfg.add_torso_mass[0], maxval=cfg.add_torso_mass[1])
-        body_mass = body_mass.at[TORSO_BODY_ID].set(
-            body_mass[TORSO_BODY_ID] + dmass
+        dmass = jax.random.uniform(
+            key, minval=cfg.add_torso_mass[0], maxval=cfg.add_torso_mass[1]
         )
+        body_mass = body_mass.at[TORSO_BODY_ID].set(body_mass[TORSO_BODY_ID] + dmass)
 
         # Jitter qpos0:
         rng, key = jax.random.split(rng)
         qpos0 = model.qpos0
         qpos0 = qpos0.at[7:].set(
             qpos0[7:]
-            + jax.random.uniform(key, shape=(12,), minval=cfg.jitter_qpos0[0], maxval=cfg.jitter_qpos0[1])
+            + jax.random.uniform(
+                key, shape=(12,), minval=cfg.jitter_qpos0[0], maxval=cfg.jitter_qpos0[1]
+            )
         )
 
         return (
@@ -81,23 +88,27 @@ def domain_randomization(sys, rng, cfg):
     ) = rand_dynamics(rng)
 
     in_axes = jax.tree_util.tree_map(lambda x: None, model)
-    in_axes = in_axes.tree_replace({
-        "geom_friction": 0,
-        "body_ipos": 0,
-        "body_mass": 0,
-        "qpos0": 0,
-        "dof_frictionloss": 0,
-        "dof_armature": 0,
-    })
+    in_axes = in_axes.tree_replace(
+        {
+            "geom_friction": 0,
+            "body_ipos": 0,
+            "body_mass": 0,
+            "qpos0": 0,
+            "dof_frictionloss": 0,
+            "dof_armature": 0,
+        }
+    )
 
-    model = model.tree_replace({
-        "geom_friction": friction,
-        "body_ipos": body_ipos,
-        "body_mass": body_mass,
-        "qpos0": qpos0,
-        "dof_frictionloss": dof_frictionloss,
-        "dof_armature": dof_armature,
-    })
+    model = model.tree_replace(
+        {
+            "geom_friction": friction,
+            "body_ipos": body_ipos,
+            "body_mass": body_mass,
+            "qpos0": qpos0,
+            "dof_frictionloss": dof_frictionloss,
+            "dof_armature": dof_armature,
+        }
+    )
 
     geom_friction = model.geom_friction[:, 0, 0]
     body_ipos = model.body_ipos[:, 1]
@@ -117,6 +128,7 @@ def domain_randomization(sys, rng, cfg):
     )
     return model, in_axes, samples
 
+
 class JointConstraintWrapper(Wrapper):
     def __init__(self, env: Env):
         super().__init__(env)
@@ -131,7 +143,7 @@ class JointConstraintWrapper(Wrapper):
     def step(self, state: State, action: jax.Array) -> State:
         state = self._env.step(state, action)
         joint_cost = self._cost_joint_pos_limits(state.data.qpos[7:])
-        state = state.replace(info={**state.info, 'cost': joint_cost})
+        state = state.replace(info={**state.info, "cost": joint_cost})
         return state
 
 
@@ -162,10 +174,12 @@ def make_joint(**kwargs):
     env = JointConstraintWrapper(env)
     return env
 
+
 def make_flip(**kwargs):
     env = locomotion.load(name, **kwargs)
     env = FlipConstraintWrapper(env)
     return env
+
 
 locomotion.register_environment(
     f"SafeJoint{name}", make_joint, locomotion.go1_joystick.default_config
@@ -173,5 +187,3 @@ locomotion.register_environment(
 locomotion.register_environment(
     f"SafeFlip{name}", make_flip, locomotion.go1_joystick.default_config
 )
-locomotion.ALL += [f"SafeJoint{name}"]
-locomotion.ALL += [f"SafeFlip{name}"]
