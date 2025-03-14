e75e187d30931e67d7c980b6977d5c19839f3939
Yarden <yarden.as@inf.ethz.ch>
2025-03-14 08:54:51 +0100

    Maybe found the bug with PPO

--- train_brax.py
+++ train_brax.py
@@ -209,16 +209,17 @@ def get_train_fn(cfg):
         penalizer, penalizer_params = get_penalizer(cfg)
         from mujoco_playground.config import locomotion_params
 
-        ppo_params = locomotion_params.brax_ppo_config(env_name)
-        ppo_training_params = dict(ppo_params)
-        if "network_factory" in ppo_params:
-            del ppo_training_params["network_factory"]
-            network_factory = functools.partial(
-                ppo_networks.make_ppo_networks, **ppo_params.network_factory
-            )
+        # ppo_params = locomotion_params.brax_ppo_config(env_name)
+        # ppo_training_params = dict(ppo_params)
+        # if "network_factory" in ppo_params:
+        #     del ppo_training_params["network_factory"]
+        #     network_factory = functools.partial(
+        #         ppo_networks.make_ppo_networks, **ppo_params.network_factory
+        #     )
         train_fn = functools.partial(
             ppo.train,
-            **dict(ppo_training_params),
+            **agent_cfg,
+            **training_cfg,
             network_factory=network_factory,
             restore_checkpoint_path=f"{get_state_path()}/ckpt",
             # wrap_env=False,  FIXME
