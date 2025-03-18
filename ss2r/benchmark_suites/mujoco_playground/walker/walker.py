f82c9ffca3d3158c53192a716c7ff59610de664c
Yarden <yarden.as@inf.ethz.ch>
2025-03-18 20:21:32 +0100

    No mass change

--- ss2r/benchmark_suites/mujoco_playground/walker/walker.py
+++ ss2r/benchmark_suites/mujoco_playground/walker/walker.py
@@ -20,11 +20,11 @@ def domain_randomization(sys, rng, cfg):
         # to help simulation stability.
         scale_factor = 3 * scale_factor / (2 * scale_factor + 1)
         geom = sys.geom_size.copy()
-        geom = geom.at[_TORSO_ID, 1].set(length)
+        # geom = geom.at[_TORSO_ID, 1].set(length)
         inertia_pos = sys.body_ipos.copy()
         inertia_pos = inertia_pos.at[_TORSO_ID, -1].add(torso_length_sample / 2.0)
-        mass = sys.body_mass.at[_TORSO_ID].multiply(scale_factor)
-        inertia = sys.body_inertia.at[_TORSO_ID].multiply(scale_factor**3)
+        # mass = sys.body_mass.at[_TORSO_ID].multiply(scale_factor)
+        # inertia = sys.body_inertia.at[_TORSO_ID].multiply(scale_factor**3)
         friction_sample = jax.random.uniform(
             rng, minval=cfg.friction[0], maxval=cfg.friction[1]
         )
