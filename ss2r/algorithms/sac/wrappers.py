715fb3be4f8067e685b9cd961e173cd914015228
Yarden As <yarden.as@inf.ethz.ch>
2025-03-19 08:42:38 +0100

    Dm walker run (#76)
    * Update params of walker walk
    
    * Add walker run
    
    * Add nominal
    
    * Add walk nominal
    
    * Remove DRs. This is a special case of PTSD with lambda=0

--- ss2r/algorithms/sac/wrappers.py
+++ ss2r/algorithms/sac/wrappers.py
@@ -117,8 +117,6 @@ class ModelDisagreement(Wrapper):
         super().__init__(env)
 
     def reset(self, rng: jax.Array) -> State:
-        # FIXME (yarden): there's a bug here.
-        # The axes are wrong.
         state = self.env.reset(rng)
         next_obs = state.info["state_propagation"]["next_obs"]
         variance = jnp.var(next_obs, axis=0).mean(-1)
