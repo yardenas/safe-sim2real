b1e670924ce435fd650b83e9667240ae07c4d92f
Yarden <yarden.as@inf.ethz.ch>
2025-03-27 18:08:40 +0100

    Improve rendering

--- paper/teaser_trajectory.py
+++ paper/teaser_trajectory.py
@@ -21,16 +21,16 @@ env_name = "Go1JoystickFlatTerrain"
 env_cfg = registry.get_default_config(env_name)
 env = registry.load(env_name, config=env_cfg)
 videos = []
-for i in range(5):
+for i in range(1):
     ep_trajectory = jax.tree_map(lambda x: x[:, i], trajectory)
     ep_trajectory = pytrees_unstack(ep_trajectory)
     video = env.render(ep_trajectory)
     videos.append(video)
-frames = np.asarray(videos).transpose(0, 1, 4, 2, 3)
+frames = np.asarray(videos)
 
 # %%
 fig, ax = plt.subplots()
-im = ax.imshow(frames[0])
+im = ax.imshow(frames[0, 0])
 
 
 def update(frame):
@@ -38,5 +38,5 @@ def update(frame):
     return [im]
 
 
-ani = animation.FuncAnimation(fig, update, frames=frames, interval=50, blit=True)
+ani = animation.FuncAnimation(fig, update, frames=frames[0], interval=50, blit=True)
 plt.show()
