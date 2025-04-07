8c7d5ecfd1e16585a6a009ecad87d26549997d95
Yarden <yarden.as@inf.ethz.ch>
2025-04-07 17:26:55 +0200

    No penalizer params

--- ss2r/algorithms/ppo/losses.py
+++ ss2r/algorithms/ppo/losses.py
@@ -238,6 +238,5 @@ def compute_ppo_loss(
         aux["constraint_estimate"] = constraint
         aux["cost_v_loss"] = cost_v_loss
         aux["ongoing_costs"] = ongoing_costs
-        aux["penalizer_params"] = penalizer_params
         aux |= penalizer_aux
     return total_loss, aux
