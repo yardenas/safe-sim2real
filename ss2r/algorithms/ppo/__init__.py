e6bb5e4bdedc5cca2e7a1dc99a5b7f7a2008f532
Yarden <yarden.as@inf.ethz.ch>
2025-05-23 10:37:34 +0200

    Update how we get agents

--- ss2r/algorithms/ppo/__init__.py
+++ ss2r/algorithms/ppo/__init__.py
@@ -1,3 +1,4 @@
+import functools
 from typing import Any, Callable, Optional, Tuple, TypeAlias
 
 import flax
@@ -8,7 +9,9 @@ from brax.training import types
 from brax.training.acme import running_statistics
 from brax.training.types import Params
 
+from ss2r.algorithms.penalizers import get_penalizer
 from ss2r.algorithms.ppo import losses as ppo_losses
+from ss2r.algorithms.sac.robustness import get_cost_robustness
 
 _PMAP_AXIS_NAME = "i"
 
@@ -37,3 +40,74 @@ TrainingStep: TypeAlias = Callable[
 TrainingStepFactory: Any
 
 InferenceParams: TypeAlias = Tuple[running_statistics.NestedMeanStd, Params]
+
+
+def get_train_fn(cfg, checkpoint_path, checkpoint_restore_path):
+    import jax.nn as jnn
+
+    from ss2r.algorithms.ppo import networks as ppo_networks
+    from ss2r.algorithms.ppo import train as ppo
+
+    agent_cfg = dict(cfg.agent)
+    training_cfg = {
+        k: v
+        for k, v in cfg.training.items()
+        if k
+        not in [
+            "render_episodes",
+            "train_domain_randomization",
+            "eval_domain_randomization",
+            "render",
+            "store_checkpoint",
+        ]
+    }
+    policy_hidden_layer_sizes = agent_cfg.pop("policy_hidden_layer_sizes")
+    value_hidden_layer_sizes = agent_cfg.pop("value_hidden_layer_sizes")
+    activation = getattr(jnn, agent_cfg.pop("activation"))
+    value_obs_key = "privileged_state" if cfg.training.value_privileged else "state"
+    policy_obs_key = "privileged_state" if cfg.training.policy_privileged else "state"
+    # FIXME (yarden): this shouldn't be here because it's related to SAC
+    cost_robustness = get_cost_robustness(cfg)
+    del training_cfg["value_privileged"]
+    del training_cfg["policy_privileged"]
+    del agent_cfg["name"]
+    if "cost_robustness" in agent_cfg:
+        del agent_cfg["cost_robustness"]
+    if "reward_robustness" in agent_cfg:
+        del agent_cfg["reward_robustness"]
+    if "penalizer" in agent_cfg:
+        del agent_cfg["penalizer"]
+    if "propagation" in agent_cfg:
+        del agent_cfg["propagation"]
+    network_factory = functools.partial(
+        ppo_networks.make_ppo_networks,
+        policy_hidden_layer_sizes=policy_hidden_layer_sizes,
+        value_hidden_layer_sizes=value_hidden_layer_sizes,
+        activation=activation,
+        value_obs_key=value_obs_key,
+        policy_obs_key=policy_obs_key,
+    )
+    penalizer, penalizer_params = get_penalizer(cfg)
+    train_fn = functools.partial(
+        ppo.train,
+        **agent_cfg,
+        **training_cfg,
+        network_factory=network_factory,
+        restore_checkpoint_path=checkpoint_restore_path,
+        penalizer=penalizer,
+        penalizer_params=penalizer_params,
+    )
+    # FIXME (yarden): that's a hack for now. Need to think of a
+    # better way to implement this.
+    if "penalizer" in cfg.agent and cfg.agent.penalizer.name == "saute":
+        train_fn = functools.partial(
+            train_fn,
+            use_saute=cfg.training.safe,
+        )
+    if "propagation" in cfg.agent and cfg.agent.propagation.name == "ts1":
+        train_fn = functools.partial(
+            train_fn,
+            use_disagreement=True,
+            disagreement_scale=cost_robustness.lambda_,
+        )
+    return train_fn
--- train_brax.py
+++ train_brax.py
@@ -8,14 +8,9 @@ import jax
 import wandb
 from omegaconf import OmegaConf
 
-import ss2r.algorithms.sac.networks as sac_networks
 from ss2r import benchmark_suites
-from ss2r.algorithms.penalizers import (
-    get_penalizer,
-)
+from ss2r.algorithms import ppo, sac
 from ss2r.algorithms.ppo.wrappers import Saute
-from ss2r.algorithms.sac.data import get_collection_fn
-from ss2r.algorithms.sac.robustness import get_cost_robustness, get_reward_robustness
 from ss2r.algorithms.sac.wrappers import ModelDisagreement, SPiDR
 from ss2r.common.logging import TrainingLogger
 
@@ -110,140 +105,17 @@ def get_train_fn(cfg):
     else:
         restore_checkpoint_dir = None
     if cfg.agent.name == "sac":
-        import jax.nn as jnn
-
-        import ss2r.algorithms.sac.train as sac
-
-        agent_cfg = dict(cfg.agent)
-        training_cfg = {
-            k: v
-            for k, v in cfg.training.items()
-            if k
-            not in [
-                "render_episodes",
-                "train_domain_randomization",
-                "eval_domain_randomization",
-                "render",
-                "store_checkpoint",
-                "value_privileged",
-                "policy_privileged",
-                "wandb_id",
-            ]
-        }
-        policy_hidden_layer_sizes = agent_cfg.pop("policy_hidden_layer_sizes")
-        value_hidden_layer_sizes = agent_cfg.pop("value_hidden_layer_sizes")
-        activation = getattr(jnn, agent_cfg.pop("activation"))
-        del agent_cfg["name"]
-        if "cost_robustness" in agent_cfg:
-            del agent_cfg["cost_robustness"]
-        if "reward_robustness" in agent_cfg:
-            del agent_cfg["reward_robustness"]
-        if "penalizer" in agent_cfg:
-            del agent_cfg["penalizer"]
-        if "propagation" in agent_cfg:
-            del agent_cfg["propagation"]
-        if "data_collection" in agent_cfg:
-            del agent_cfg["data_collection"]
-        value_obs_key = "privileged_state" if cfg.training.value_privileged else "state"
-        policy_obs_key = (
-            "privileged_state" if cfg.training.policy_privileged else "state"
-        )
-        network_factory = functools.partial(
-            sac_networks.make_sac_networks,
-            policy_hidden_layer_sizes=policy_hidden_layer_sizes,
-            value_hidden_layer_sizes=value_hidden_layer_sizes,
-            activation=activation,
-            value_obs_key=value_obs_key,
-            policy_obs_key=policy_obs_key,
-        )
-        penalizer, penalizer_params = get_penalizer(cfg)
-        cost_robustness = get_cost_robustness(cfg)
-        reward_robustness = get_reward_robustness(cfg)
-        # TODO (yarden): refactor all of these things to be algo dependent.
-        # Create algos in their init
-        data_collection = get_collection_fn(cfg)
-        train_fn = functools.partial(
-            sac.train,
-            **agent_cfg,
-            **training_cfg,
-            network_factory=network_factory,
-            checkpoint_logdir=get_state_path(),
-            cost_robustness=cost_robustness,
-            reward_robustness=reward_robustness,
-            penalizer=penalizer,
-            penalizer_params=penalizer_params,
-            get_experience_fn=data_collection,
-            restore_checkpoint_path=restore_checkpoint_dir,
+        train_fn = sac.get_train_fn(
+            cfg,
+            restore_checkpoint_dir=restore_checkpoint_dir,
+            checkpoint_path=get_state_path(),
         )
     elif cfg.agent.name == "ppo":
-        import jax.nn as jnn
-
-        from ss2r.algorithms.ppo import networks as ppo_networks
-        from ss2r.algorithms.ppo import train as ppo
-
-        agent_cfg = dict(cfg.agent)
-        training_cfg = {
-            k: v
-            for k, v in cfg.training.items()
-            if k
-            not in [
-                "render_episodes",
-                "train_domain_randomization",
-                "eval_domain_randomization",
-                "render",
-                "store_checkpoint",
-            ]
-        }
-        policy_hidden_layer_sizes = agent_cfg.pop("policy_hidden_layer_sizes")
-        value_hidden_layer_sizes = agent_cfg.pop("value_hidden_layer_sizes")
-        activation = getattr(jnn, agent_cfg.pop("activation"))
-        value_obs_key = "privileged_state" if cfg.training.value_privileged else "state"
-        policy_obs_key = (
-            "privileged_state" if cfg.training.policy_privileged else "state"
+        train_fn = ppo.get_train_fn(
+            cfg,
+            restore_checkpoint_dir=restore_checkpoint_dir,
+            checkpoint_path=get_state_path(),
         )
-        cost_robustness = get_cost_robustness(cfg)
-        del training_cfg["value_privileged"]
-        del training_cfg["policy_privileged"]
-        del agent_cfg["name"]
-        if "cost_robustness" in agent_cfg:
-            del agent_cfg["cost_robustness"]
-        if "reward_robustness" in agent_cfg:
-            del agent_cfg["reward_robustness"]
-        if "penalizer" in agent_cfg:
-            del agent_cfg["penalizer"]
-        if "propagation" in agent_cfg:
-            del agent_cfg["propagation"]
-        network_factory = functools.partial(
-            ppo_networks.make_ppo_networks,
-            policy_hidden_layer_sizes=policy_hidden_layer_sizes,
-            value_hidden_layer_sizes=value_hidden_layer_sizes,
-            activation=activation,
-            value_obs_key=value_obs_key,
-            policy_obs_key=policy_obs_key,
-        )
-        penalizer, penalizer_params = get_penalizer(cfg)
-        train_fn = functools.partial(
-            ppo.train,
-            **agent_cfg,
-            **training_cfg,
-            network_factory=network_factory,
-            restore_checkpoint_path=restore_checkpoint_dir,
-            penalizer=penalizer,
-            penalizer_params=penalizer_params,
-        )
-        # FIXME (yarden): that's a hack for now. Need to think of a
-        # better way to implement this.
-        if "penalizer" in cfg.agent and cfg.agent.penalizer.name == "saute":
-            train_fn = functools.partial(
-                train_fn,
-                use_saute=cfg.training.safe,
-            )
-        if "propagation" in cfg.agent and cfg.agent.propagation.name == "ts1":
-            train_fn = functools.partial(
-                train_fn,
-                use_disagreement=True,
-                disagreement_scale=cost_robustness.lambda_,
-            )
     else:
         raise ValueError(f"Unknown agent name: {cfg.agent.name}")
     return train_fn
