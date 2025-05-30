normalization fixed for observations - rewards and cost outputs unnormalized

ss2r/algorithms/mb_ppo/losses.py
@@ -80,6 +80,7 @@ def compute_gae(
 
 def make_losses(
     ppo_network,
+    preprocess_observations_fn,
     clipping_epsilon,
     entropy_cost,
     reward_scaling,
@@ -87,7 +88,6 @@ def make_losses(
     gae_lambda,
     normalize_advantage,
     cost_scaling,
-    safety_budget,
     safety_discounting,
     safety_gae_lambda,
 ):
@@ -125,17 +125,21 @@ def make_losses(
         learn_std,
     ):
         model_apply = ppo_network.model_network.apply
+        expand = lambda x: jnp.tile(
+            x[None], (next_obs_pred.shape[0],) + (1,) * (x.ndim)
+        )
         (
             (next_obs_pred, reward_pred, cost_pred),
             (next_obs_std, reward_std, cost_std),
         ) = model_apply(normalizer_params, model_params, data.observation, data.action)
-        expand = lambda x: jnp.tile(
-            x[None], (next_obs_pred.shape[0],) + (1,) * (x.ndim)
+        next_obs_pred = preprocess_observations_fn(next_obs_pred, normalizer_params)
+        next_obs_target = preprocess_observations_fn(
+            expand(data.next_observation), normalizer_params
         )
-        next_obs_target = expand(data.next_observation)
         next_obs_loss = _neg_log_posterior(
             next_obs_pred, next_obs_std, next_obs_target, learn_std
         )
+        # FIXME: (manu) no normalization of reward and cost?
         current_reward_target = expand(data.reward)
         reward_loss = _neg_log_posterior(
             reward_pred, reward_std, current_reward_target, learn_std

ss2r/algorithms/mb_ppo/networks.py
@@ -95,6 +95,7 @@ def make_world_model_ensemble(
     obs_size: int,
     action_size: int,
     preprocess_observations_fn: types.PreprocessObservationFn = types.identity_observation_preprocessor,
+    postprocess_observations_fn: types.PreprocessObservationFn = types.identity_observation_preprocessor,
     hidden_layer_sizes: Sequence[int] = (256, 256),
     activation: networks.ActivationFn = linen.relu,
     n_ensemble: int = 5,
@@ -136,31 +137,35 @@ def make_world_model_ensemble(
 
     model = MModule(n_ensemble=n_ensemble, obs_size=obs_size)
 
-    def apply(processor_params, params, obs, actions):
-        obs_processed = preprocess_observations_fn(obs, processor_params)
+    def apply(preprocessor_params, params, obs, actions):
+        obs_processed = preprocess_observations_fn(obs, preprocessor_params)
         raw_output = model.apply(params, obs_processed, actions)
         # Std devs also need to match the shape (B, E, feature_dim)
         if not learn_std:
-            obs, reward, cost = (
+            obs_raw, reward, cost = (
                 raw_output[..., :obs_size],
                 raw_output[..., obs_size],
                 raw_output[..., obs_size + 1],
             )
+            obs = postprocess_observations_fn(obs_raw, preprocessor_params)
             obs_std = jnp.ones_like(obs) * 1e-3
             reward_std = jnp.ones_like(reward) * 1e-3
             cost_std = jnp.ones_like(cost) * 1e-3
         else:
             means, stds = jnp.split(raw_output, 2, axis=-1)
-            obs, reward, cost = (
+            obs_raw, reward, cost = (
                 means[..., :obs_size],
                 means[..., obs_size],
                 means[..., obs_size + 1],
             )
+            obs = postprocess_observations_fn(obs_raw, preprocessor_params)
+
             obs_std, reward_std, cost_std = (
                 stds[..., :obs_size],
                 stds[..., obs_size],
                 stds[..., obs_size + 1],
             )
+            # FIXME: (manu) figure out postprocessing of stds (only take scaling part of postprocessor)
         return (obs, reward, cost), (obs_std, reward_std, cost_std)
 
     dummy_obs = jnp.zeros((1, obs_size))
@@ -174,11 +179,12 @@ def make_mb_ppo_networks(
     observation_size: int,
     action_size: int,
     preprocess_observations_fn: types.PreprocessObservationFn = types.identity_observation_preprocessor,
+    postprocess_observations_fn: types.PreprocessObservationFn = types.identity_observation_preprocessor,
     policy_hidden_layer_sizes: Sequence[int] = (32,) * 4,
     value_hidden_layer_sizes: Sequence[int] = (256,) * 5,
     model_hidden_layer_sizes: Sequence[int] = (512,) * 2,
     n_ensemble: int = 5,
-    model_use_bro: bool = True,
+    use_bro: bool = True,
     activation: networks.ActivationFn = linen.swish,
     policy_obs_key: str = "state",
     value_obs_key: str = "state",
@@ -222,10 +228,11 @@ def make_mb_ppo_networks(
         observation_size,
         action_size,
         preprocess_observations_fn=preprocess_observations_fn,
+        postprocess_observations_fn=postprocess_observations_fn,
         hidden_layer_sizes=model_hidden_layer_sizes,
         activation=activation,
         n_ensemble=n_ensemble,
-        use_bro=model_use_bro,
+        use_bro=use_bro,
         learn_std=learn_std,
     )
 

ss2r/algorithms/mb_ppo/test_train_model.py
@@ -115,14 +115,19 @@ def test_train_model():
         actions.shape[1] if isinstance(actions, jnp.ndarray) else actions.shape
     )
 
+    normalize_fn = running_statistics.normalize
+    denormalize_fn = running_statistics.denormalize
+
     # Create a dummy model
     ppo_network = mb_ppo_networks.make_mb_ppo_networks(
         observation_size=obs_size,
         action_size=action_size,
+        preprocess_observations_fn=normalize_fn,
+        postprocess_observations_fn=denormalize_fn,
         n_ensemble=1,
         model_hidden_layer_sizes=[512, 512],
         activation=jax.nn.relu,
-        model_use_bro=False,
+        use_bro=True,
         learn_std=False,
     )
 
@@ -149,6 +154,7 @@ def test_train_model():
     # Make loss
     model_loss, *_ = mb_ppo_losses.make_losses(
         ppo_network=ppo_network,
+        preprocess_observations_fn=normalize_fn,
         entropy_cost=1e-4,
         discounting=0.9,
         safety_discounting=0.9,
@@ -158,7 +164,6 @@ def test_train_model():
         safety_gae_lambda=0.95,
         clipping_epsilon=0.3,
         normalize_advantage=True,
-        safety_budget=float("inf"),
     )
 
     # Make gradient function
@@ -232,7 +237,7 @@ def test_train_model():
         return carry, mse_hist
 
     carry, mse_hist = train_model(
-        params, optimizer_state, transitions, normalizer_params, key_sgd, num_epochs=100
+        params, optimizer_state, transitions, normalizer_params, key_sgd, num_epochs=50
     )
 
     plt.plot(mse_hist[0], label="MSE Next Obs")