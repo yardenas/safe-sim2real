afe33343427cf304ac2a875ef0a2c6c5507a1034
Yarden <yarden.as@inf.ethz.ch>
2025-06-09 15:32:56 +0200

    take the mean

--- ss2r/algorithms/sac/losses.py
+++ ss2r/algorithms/sac/losses.py
@@ -147,7 +147,9 @@ def make_losses(
         if use_bro:
             qr = jnp.mean(qr_action, axis=-1)
         else:
-            qr = jnp.min(qr_action, axis=-1)
+            # FIXME (take the mean)
+            # qr = jnp.min(qr_action, axis=-1)
+            qr = jnp.mean(qr_action, axis=-1)
         aux = {}
         actor_loss = -qr.mean()
         exploration_loss = (alpha * log_prob).mean()
