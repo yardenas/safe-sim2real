7cb9aa3c79017eb9f9552e284135deebfd4aa832
ManuelWendl <manuel.r.wendl@gmail.com>
2025-06-26 18:39:36 +0200

    get scaling fn back

--- ss2r/algorithms/mbpo/safe_rollout.py
+++ ss2r/algorithms/mbpo/safe_rollout.py
@@ -66,7 +66,9 @@ def make_safe_inference_fn(
                 raise ValueError("QC network is not defined, cannot do shielding.")
             accumulated_cost = observations["cumulative_cost"][:, 0]
             current_discount = observations["curr_discount"][:, 0]
-            expected_total_cost = accumulated_cost + qc * current_discount * qc_scale
+            expected_total_cost = (
+                scaling_fn(accumulated_cost) + qc * current_discount * qc_scale
+            )
             backup_action = backup_policy(observations, key_sample)[0]
             safe = expected_total_cost[:, None] < safety_budget
             safe_action = jnp.where(
