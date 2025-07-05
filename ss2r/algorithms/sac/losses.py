4b6a2780e51d236bd2fb320510e32b90ee9731a6
Yarden As <yarden.as@inf.ethz.ch>
2025-07-05 08:14:46 +0200

    Fix init alpha (#142)

--- ss2r/algorithms/sac/losses.py
+++ ss2r/algorithms/sac/losses.py
@@ -39,11 +39,10 @@ def make_losses(
     safety_discounting: float,
     action_size: int,
     use_bro: bool,
-    init_alpha: float | None,
 ):
     """Creates the SAC losses."""
 
-    target_entropy = -0.5 * action_size if init_alpha is None else init_alpha
+    target_entropy = -0.5 * action_size
     policy_network = sac_network.policy_network
     qr_network = sac_network.qr_network
     qc_network = sac_network.qc_network
