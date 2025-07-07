49c3b8610ca848f221b9c83aade5d9eb7dbd86a6
Yarden <yarden.as@inf.ethz.ch>
2025-07-07 18:54:56 +0200

    Fix bugs

--- ss2r/algorithms/sac/train.py
+++ ss2r/algorithms/sac/train.py
@@ -245,10 +245,8 @@ def train(
     if isinstance(obs_size, Mapping):
         for key, value in obs_size.items():
             if key.startswith("pixels/") and len(value) > 3 and value[0] == 1:
-                print("heheheheheeh")
                 value = value[1:]
                 obs_size[key] = value  # type: ignore
-    print("afterere", obs_size)
     action_size = env.action_size
     normalize_fn = lambda x, y: x
     if normalize_observations:
