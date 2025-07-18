889d6e9f143d5bb20350070d5c736c30a82e8596
Yarden <yarden.as@inf.ethz.ch>
2025-07-18 22:13:33 +0200

    Load newer policy

--- ss2r/algorithms/sac/train.py
+++ ss2r/algorithms/sac/train.py
@@ -330,7 +330,6 @@ def train(
     local_key, rb_key, env_key, eval_key = jax.random.split(local_key, 4)
     if restore_checkpoint_path is not None:
         params = checkpoint.load(restore_checkpoint_path)
-        # FIXME (yarden): del params[-1]
         policy_optimizer_state = update_lr_schedule_count(
             restore_state(params[6], training_state.policy_optimizer_state), 0
         )
