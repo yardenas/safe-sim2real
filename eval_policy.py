cc002326c50a4111085db550f10f47b74dceb501
Anton Pollak <antoncarlospollak@gmail.com>
2025-07-27 06:42:52 +0200

    implement averaging over multiple evaluations with different seeds

--- eval_policy.py
+++ eval_policy.py
@@ -14,6 +14,23 @@ from train_brax import _validate_madrona_args
 
 _LOG = logging.getLogger(__name__)
 
+def average_metrics(metrics: list) -> dict:
+
+    n_runs = len(metrics)
+    if n_runs == 0:
+        raise ValueError("Empty metrics list passed to function.")
+    
+    averaged_metrics = {}
+    for metric in metrics[0].keys():
+        avg_value = 0
+        for i in range(n_runs):
+            avg_value += metrics[i][metric]
+        avg_value /= n_runs
+        averaged_metrics[metric] = avg_value
+
+    return averaged_metrics
+
+
 def get_policy_fn(cfg):
     if cfg.training.wandb_id:
         checkpoint_path = get_wandb_checkpoint(cfg.training.wandb_id, cfg.wandb.entity)
@@ -38,6 +55,14 @@ def main(cfg):
     # Avoid training by setting the number of timesteps to 0
     cfg.training.num_timesteps = 0
     cfg.agent.min_replay_size = -1
+
+    if cfg.wandb.name_notes:
+        notes = cfg.wandb.name_notes + '_'
+    else:
+        notes = ''
+
+    cfg.wandb.name = 'eval_' + notes + cfg.wandb.name
+
     logger = TrainingLogger(cfg)
 
     # --- wandb setup ---
@@ -46,37 +71,55 @@ def main(cfg):
         import wandb
         wandb.init(
             project=getattr(cfg, "wandb", {}).get("project", "ss2r"),
-            # entity=getattr(cfg, "wandb", {}).get("entity", None),
             config=OmegaConf.to_container(cfg, resolve=True),
             resume=True,
             **getattr(cfg, "wandb", {}),
         )
         wandb_handle = wandb
 
-    train_fn = get_policy_fn(cfg)
-    train_env_wrap_fn, eval_env_wrap_fn = benchmark_suites.get_wrap_env_fn(cfg)
-    use_vision = "use_vision" in cfg.agent and cfg.agent.use_vision
-    train_env, eval_env = benchmark_suites.make(cfg, train_env_wrap_fn, eval_env_wrap_fn)
-    if use_vision:
-        _validate_madrona_args(
-            train_env,
-            eval_env,
-            cfg.training.num_envs,
-            cfg.training.num_eval_envs,
-            cfg.training.action_repeat,
-            cfg.environment.task_params.vision_config.render_batch_size,
-        )
-    with jax.disable_jit(not cfg.jit):
-        make_policy, params, metrics = train_fn(
-            environment=train_env,
-            eval_env=eval_env,
-            progress_fn=lambda *args, **kwargs: None,
-        )
-        _LOG.info("------------Metrics:")
-        _LOG.info(metrics)
-        # Log metrics to wandb if enabled
-        if wandb_handle is not None and metrics:
-            wandb_handle.log(metrics)
+    # Create multiple runs using seeds
+    try:
+        seeds = range(cfg.eval_params.num_seeds)
+    except:
+        raise ValueError("Could not retrieve number of different seeds from config parameter 'eval_params.num_seeds'. Please provide it.")
+
+    metrics = []
+    for i, seed in enumerate(seeds):
+        cfg.training.seed = seed
+
+        train_fn = get_policy_fn(cfg)
+        train_env_wrap_fn, eval_env_wrap_fn = benchmark_suites.get_wrap_env_fn(cfg)
+        use_vision = "use_vision" in cfg.agent and cfg.agent.use_vision
+        train_env, eval_env = benchmark_suites.make(cfg, train_env_wrap_fn, eval_env_wrap_fn)
+        if use_vision:
+            _validate_madrona_args(
+                train_env,
+                eval_env,
+                cfg.training.num_envs,
+                cfg.training.num_eval_envs,
+                cfg.training.action_repeat,
+                cfg.environment.task_params.vision_config.render_batch_size,
+            )
+        with jax.disable_jit(not cfg.jit):
+            make_policy, params, metrics_for_run = train_fn(
+                environment=train_env,
+                eval_env=eval_env,
+                progress_fn=lambda *args, **kwargs: None,
+            )
+        _LOG.info(f"------Metrics for evaluation run {i}:")
+        _LOG.info(metrics_for_run)
+        metrics += [metrics_for_run]
+
+    averaged_metrics = average_metrics(metrics)
+
+    _LOG.info(f"------------Metrics averaged over all {cfg.training.num_seeds} runs:")
+    _LOG.info(metrics_for_run)
+    
+    averaged_metrics['num_seeds'] = cfg.eval_params.num_seeds
+    averaged_metrics['seeds'] = list(seeds)
+    # Log metrics to wandb if enabled
+    if wandb_handle is not None and averaged_metrics:
+        wandb_handle.log(averaged_metrics)
     _LOG.info("Done evaluating.")
 
 if __name__ == "__main__":
