c243f95906b698fbca17bb95a287cb00fd54ce9f
Yarden <yarden.as@inf.ethz.ch>
2025-08-01 16:09:13 +0200

    Load params correctly

--- ss2r/algorithms/mbpo/train.py
+++ ss2r/algorithms/mbpo/train.py
@@ -368,19 +368,20 @@ def train(
             )
         if load_auxiliaries:
             policy_optimizer_state = restore_state(
-                params[6], training_state.behavior_policy_optimizer_state
+                params[6][1]["inner_state"],
+                training_state.behavior_policy_optimizer_state,
             )
             alpha_optimizer_state = restore_state(
-                params[7], training_state.alpha_optimizer_state
+                params[7][1], training_state.alpha_optimizer_state
             )
             qr_optimizer_state = restore_state(
-                params[8], training_state.behavior_qr_optimizer_state
+                params[8][1], training_state.behavior_qr_optimizer_state
             )
             if qc_optimizer is None:
                 qc_optimizer_state = None
             else:
                 qc_optimizer_state = restore_state(
-                    params[9], training_state.backup_qc_optimizer_state
+                    params[9][1], training_state.backup_qc_optimizer_state
                 )
             training_state = training_state.replace(  # type: ignore
                 behavior_policy_optimizer_state=policy_optimizer_state,
